90cc4959f0eb350e8b2a36a71d4f191828fb2d5b
eyabc <bey4314@naver.com>
2020-03-31 22:30:05 +0900

    settings

---
+++ ms_vue/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="site">
+    <site-header></site-header>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+    <site-footer id="site-footer"></site-footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+// import { Action, State } from 'vuex-class';
+import { SiteHeader, SiteFooter } from '@/components';
+import '@/assets/scss/common.scss'
+
+const components = { SiteFooter, SiteHeader };
+@Component({ components })
+export default class App extends Vue {
+
+}
+</script>
+<style lang="scss" >
+#site {
+  &-body {
+  }
+  &-footer {
+    background: #424242;
+    color: #adadad;
+    font-size: 12px;
+    padding: 0;
+    bottom: 0;
+    left: 0;
+    position: fixed;
+    width: 100%;
+    text-align: center;
+  }
+}
+</style>
---
+++ ms_vue/src/middleware/router/index.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import { Landing } from "@/views";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Landing',
+    component: Landing,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router
