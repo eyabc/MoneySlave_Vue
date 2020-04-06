d5c278563abb449f883fdb947a35297b4a75bb52
junilHwang <tjsdlf4261@naver.com>
2020-04-06 13:34:25 +0900

    [준일] 코드 리뷰 중

--- ms_vue/src/middleware/icon/index.ts
+++ ms_vue/src/middleware/icon/index.ts
@@ -2,9 +2,9 @@ import Vue from 'vue';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
-import {faWonSign} from '@fortawesome/free-solid-svg-icons';
+import { faWonSign } from '@fortawesome/free-solid-svg-icons';
 
-const icons = [faWonSign];
+const icons = [ faWonSign ];
 
 library.add(...icons);
 
