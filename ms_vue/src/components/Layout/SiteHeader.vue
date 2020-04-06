2c7f5553a61e264f4e128aa092bb730252550023
junilHwang <tjsdlf4261@naver.com>
2020-04-06 13:53:02 +0900

    [준일] User Store 추가

--- ms_vue/src/components/Layout/SiteHeader.vue
+++ ms_vue/src/components/Layout/SiteHeader.vue
@@ -4,18 +4,18 @@
             <div class="container">
                 <!-- TODO link when user to '/schedule; or '/' -->
                 <span id="logo"><fa icon="won-sign"/></span>
-                <span v-if="!user">
+                <span v-if="userInfo === null">
                     <el-button icon="login" size="mini" @click="handleDialogSignIn" plain>로그인</el-button>
                 </span>
                 <el-dropdown trigger="click" v-else>
-                    <span class="el-dropdown-link"><b>{{username}}</b> 님 오늘하루도 화이팅<i class="el-icon-arrow-down el-icon--right"></i></span>
+                    <span class="el-dropdown-link"><b>{{userInfo.name}}</b> 님 오늘하루도 화이팅<i class="el-icon-arrow-down el-icon--right"></i></span>
                     <el-dropdown-menu slot="dropdown" class="dropdown">
                         <!-- TODO link to menu -->
-                        <el-dropdown-item disabled>{{userid}}</el-dropdown-item>
+                        <el-dropdown-item disabled>{{userInfo.id}}</el-dropdown-item>
                         <el-dropdown-item>계좌</el-dropdown-item>
                         <el-dropdown-item>급여</el-dropdown-item>
                         <el-dropdown-item>내 정보</el-dropdown-item>
-                        <el-dropdown-item>로그아웃</el-dropdown-item>
+                        <el-dropdown-item @click.native="logout">로그아웃</el-dropdown-item>
                     </el-dropdown-menu>
                 </el-dropdown>
             </div>
@@ -25,21 +25,39 @@
 </template>
 
 <script lang="ts">
-    import {Vue, Component} from 'vue-property-decorator'
-    import {SignIn} from '@/components'
+import { Vue, Component } from 'vue-property-decorator';
+import { SignIn } from '@/components';
+import { namespace } from "vuex-class";
+import { UserInfo } from "@/middleware/store/user";
+import {MutationMethod} from "vuex";
 
-    const components = {SignIn};
-    @Component({components})
-    export default class SiteHeader extends Vue {
-        user: any = true;
-        username?: string = '박은영';
-        userid?: string = 'bey4314@naver.com';
+const userStore = namespace('userStore');
 
-        handleDialogSignIn() {
-            const signInForm: any = this.$refs.signInForm;
-            signInForm.open();
-        }
+const components = {SignIn};
+@Component({components})
+export default class SiteHeader extends Vue {
+
+    @userStore.State userInfo!: UserInfo|null;
+    @userStore.Mutation setUserInfo!: MutationMethod;
+
+    private handleDialogSignIn() {
+        const signInForm: any = this.$refs.signInForm;
+        signInForm.open();
     }
+
+    private logout () {
+        this.$message({ type: 'success', message: '로그아웃 되었습니다' })
+        this.setUserInfo(null)
+        console.log(this.userInfo);
+    }
+
+    private created () {
+        this.setUserInfo({
+            id: 'bey4314@naver.com',
+            name: '박은영'
+        })
+    }
+}
 </script>
 
 <style lang="scss">
